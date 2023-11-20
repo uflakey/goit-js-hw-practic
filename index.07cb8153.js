e71f3fb8d1f08e4f516084fea7a22d8c6fdf91ed
uflakey <134938478+uflakey@users.noreply.github.com>
2023-11-20 22:03:15 +0000

    Deploying to gh-pages from @ uflakey/goit-js-hw-practic@e4645d0de3ee06e1bed868c03d8ee2a647d01286 🚀

--- index.07cb8153.js
+++
@@ -1,2 +0,0 @@
-!function(){var n=Date.now();setInterval((function(){var o=Date.now();console.log("start => currentTime",o),console.log(o-n)}),1e3)}();
-//# sourceMappingURL=index.07cb8153.js.map
--- index.a0be23b9.js
+++
@@ -1,2 +0,0 @@
-({start(){const t=Date.now();setInterval((()=>{const o=Date.now();console.log("start => currentTime",o),console.log(o-t)}),1e3)}}).start();
-//# sourceMappingURL=index.a0be23b9.js.map

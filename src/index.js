e4645d0de3ee06e1bed868c03d8ee2a647d01286
uflakey <134938478+uflakey@users.noreply.github.com>
2023-11-21 00:02:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,71 @@
-const timer = {
-  start() {
-    const startTime = Date.now();
+// таймер
+// const timer = {
+//   start() {
+//     const startTime = Date.now();
 
-    setInterval(() => {
-      const currentTime = Date.now();
-      console.log('start => currentTime', currentTime);
-      console.log(currentTime - startTime);
-    }, 1000);
-  },
-};
+//     setInterval(() => {
+//       const currentTime = Date.now();
+//       const deltaTime = currentTime - startTime;
+//       const { hours, mins, secs } = getTimeComponents(deltaTime);
 
-timer.start();
+//       console.log(`${hours}:${mins}:${secs}`);
+//     }, 1000);
+//   },
+// };
+
+// timer.start();
+
+// function updateClockfase({ hours, mins, secs }) {
+//   refs.clockfase.textContent = `${hours}, ${mins}, ${secs}`;
+// }
+
+// function pad(value) {
+//   return String(value).padStart(2, '0');
+// }
+
+// function getTimeComponents(time) {
+//   const hours = pad(
+//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+//   );
+//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+//   return { hours, mins, secs };
+// }
+
+// // Проміси
+// const promise = new Promise((resolve, rejekt) => {
+//   const canFulfill = Math.random() > 0.5;
+//   console.log(canFulfill);
+
+//   setTimeout(() => {
+//     if (canFulfill) {
+//       resolve('Проміс виконано успішно');
+//     }
+//     rejekt('Проміс відхилено');
+//   }, 2000);
+// });
+
+// promise.then(onFulfilled, onRejekted);
+
+// function onFulfilled(result) {
+//   console.log('onFulfilled => onFulfilled');
+//   console.log(`${result}`);
+// }
+// function onRejekted(error) {
+//   console.log('onRejekted => onRejekted');
+//   console.log(`${error}`);
+// }
+
+// Запити на бекенд
+// fetch('https://pokeapi.co/api/v2/pokemon/4')
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(pokemon => {
+//     console.log(pokemon);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   })
+//   .finally(() => reset());
